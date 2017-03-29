88fc2c61739898dce89570fbc1c223f5ffa2d9ae
Deborah Perez <Deborah.a.perez@gmail.com>
2017-03-29 12:39:31 -0400

    Began the process of finding the linear mass spectrum of a peptide

--- linearspectrum.py
+++ linearspectrum.py
@@ -23,10 +23,41 @@ aminoAcidMassTable = {'G': 57, 'A': 71, 'S': 87, 'P': 97, 'V': 99,
 def linear_spectrum(peptide, aminoAcidMassTable):
     prefixMass = []
     prefixMass.append(0)
-    print (prefixMass)
-    return aminoAcidMassTable
+    aminoAcidList = list(aminoAcidMassTable)
+    peptideLength = len(peptide)
+    print ('amino acid list:', aminoAcidList)
+    print ('\nprefix mass list:', prefixMass,
+     '\nlength of amino acid mass table:', len(aminoAcidMassTable), '\n')
+    for i in range(1, peptideLength + 1):
+        currentPeptideIndex = i-1
+        aminoAcid = peptide[currentPeptideIndex]
+        aminoAcidMass = aminoAcidMassTable[aminoAcid]
+        for j in range(len(aminoAcidList)):
+            aminoAcidOfJ = aminoAcidList[j]
+            if aminoAcidOfJ == aminoAcid:
+                prefixOfI = prefixMass[i-1] + aminoAcidMass
+                prefixMass.append(prefixOfI)
+                print ('i:', i,
+                '\nCurrent peptide index:', currentPeptideIndex,
+                '\ni minus 1:', i-1,
+                '\n prefixMass of i - 1:', prefixMass[i-1],
+                '\nAmino acid:', aminoAcid, 'aminoAcidMass:', aminoAcidMass, '\nj:', j,
+                '\naminoAcidOfJ:', aminoAcidOfJ, '\nprefixOfI:', prefixOfI,
+                '\nCurrent prefixMass List:', prefixMass, '\n')
+    linearSpectrum = [0]
+
+#    print ('pseudocode left to complete:\npeptideMass ← PrefixMass(|Peptide|)',
+#    '\nCyclicSpectrum ← a list consisting of the single integer 0',
+#    '\n    for i ← 0 to |Peptide| − 1',
+#    '\n        for j ← i + 1 to |Peptide|',
+#    '\n            add PrefixMass(j) − PrefixMass(i) to CyclicSpectrum',
+#    '\n            if i > 0 and j < |Peptide|',
+#    '\n                add peptideMass - (PrefixMass(j) − PrefixMass(i)) to CyclicSpectrum',
+#    '\n    return sorted list CyclicSpectrum')
+    return prefixMass
 # ----------------------------------------------------------------------
 # ---MAINCODE------------------------------------------------------------
-peptide = 'NQEL'
+rawData = sys.stdin.read().splitlines()
+peptide = rawData[0]
 linearSpectrum = linear_spectrum(peptide, aminoAcidMassTable)
 print (linearSpectrum)
